abe2debf47b505b255aad3ce75be84be70c598d8
kimdaeyoon <89075605+apeachicetea@users.noreply.github.com>
2021-12-01 19:43:24 +0900

    typescript state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
-import Circle from "./Circle";
+
 
 function App() {
 
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow"/>
-      <Circle bgColor="tomato" text="Second"/>
+      <h1>Hello~</h1>
     </div>
   );
 }
