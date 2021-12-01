a23d43bce6967750961d0e7c22530bd1621ddd9e
kimdaeyoon <89075605+apeachicetea@users.noreply.github.com>
2021-12-01 20:39:20 +0900

    typescript event

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
 
 function App() {
+  const [value, setValue] = useState("");
+
+  function onChange (event: React.FormEvent<HTMLInputElement>){
+    // console.log(event);
+    // console.log(event.currentTarget);
+    // console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value }
+    } = event;
+    setValue(event.currentTarget.value);
+  }
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  }
 
   return (
     <div>
-      <h1>Hello~</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          placeholder="username" 
+          value={value} 
+          onChange={onChange}
+        />
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
