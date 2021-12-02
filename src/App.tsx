d926d97d61675bcb79ddf1a0b4cfcd001d7e9eb7
kimdaeyoon <89075605+apeachicetea@users.noreply.github.com>
2021-12-02 17:25:21 +0900

    typescript&styledcomponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,20 @@
-import React, { useState } from "react";
 import styled from "styled-components";
 
 
 function App() {
-  const [value, setValue] = useState("");
+  
+  const Container = styled.div`
+    background-color: ${props => props.theme.bgColor};
+  `;
 
-  function onChange (event: React.FormEvent<HTMLInputElement>){
-    // console.log(event);
-    // console.log(event.currentTarget);
-    // console.log(event.currentTarget.value);
-    const {
-      currentTarget: { value }
-    } = event;
-    setValue(event.currentTarget.value);
-  }
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  }
+  const H1 = styled.div`
+    color: ${props => props.theme.textColor}
+  `;
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input 
-          type="text" 
-          placeholder="username" 
-          value={value} 
-          onChange={onChange}
-        />
-        <button>Log In</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Hello</H1>
+    </Container>
   );
 }
 
