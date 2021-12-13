9f55c00ceff5a5c176413293cd201ffca8b3d0e3
kimdaeyoon <eodbszla@gmail.com>
2021-12-13 16:58:27 +0900

    Home part

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-function Coins() {
-
-  const Container = styled.div`
+const Container = styled.div`
     padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
   `;
 
   const Header = styled.header`
@@ -24,7 +25,8 @@ function Coins() {
     border-radius: 15px;
     a {
       transition: color 0.2s ease-in;
-      display: block;
+      display: flex;
+      align-items: center;
       padding: 20px;
     }
     &:hover {
@@ -39,50 +41,70 @@ function Coins() {
     color: ${props => props.theme.accentColor};
   `;
 
-  const coins = [
-    {
-      id: "btc-bitcoin",
-      name: "Bitcoin",
-      symbol: "BTC",
-      rank: 1,
-      is_new: false,
-      is_active: true,
-      type: "coin",
-      },
-      {
-      id: "eth-ethereum",
-      name: "Ethereum",
-      symbol: "ETH",
-      rank: 2,
-      is_new: false,
-      is_active: true,
-      type: "coin",
-      },
-      {
-      id: "hex-hex",
-      name: "HEX",
-      symbol: "HEX",
-      rank: 3,
-      is_new: false,
-      is_active: true,
-      type: "token",
-      },
-  ]
+  const Loader = styled.span`
+    text-align: center;
+    display: block;
+    font-size: 30px;
+    margin-top: 50px;
+  `;
+
+  const Img = styled.img`
+    height: 25px;
+    width: 25px;
+    margin-right: 10px;
+  `;
+
+  interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+  }
+
+function Coins() {
+  
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+
+  useEffect(() => {
+    (async() => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      // console.log(json);
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, [])
 
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinsList>
+      {
+      loading
+      ? <Loader>Loading...</Loader>
+      : <CoinsList>
         { 
           coins.map((el)=>(
             <Coin key={el.id}>
-              <Link to={`/${el.id}`}>{el.name} &rarr;</Link>
+              <Link 
+                to={{
+                  pathname: `/${el.id}`,
+                  state: { name: el.name }
+                }}
+              >
+                <Img src={`https://cryptoicon-api.vercel.app/api/icon/${el.symbol.toLowerCase()}`} />
+                {el.name} &rarr;
+              </Link>
             </Coin>)
           )
         }
-      </CoinsList>
+        </CoinsList>
+      }
     </Container>
   )
 }
